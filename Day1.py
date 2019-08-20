32bd1683fa428db7d8d369048afd7e2c91711e0c
Kuser <almaari.eman@gmail.com>
2019-08-21 00:15:49 +0300

    First Day Task

---
+++ Day1.py
@@ -0,0 +1,12 @@
+#Printing Statements
+print('Hello, World!')
+print('This is the first task of 100days_of_code')
+
+#Indentation in Python is very important to indicate a block of code
+if 5>2:
+    print('Five is greater than Two!')
+	
+#In the above code, the print statement inside if block.
+#if you skip the indentation, we will get error
+if 5>2:
+print('Five is greater than Two!') #IndentationError: expected an indented block
