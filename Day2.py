6715b2812617ea0e1ea302137821c820f642da65
Kuser <almaari.eman@gmail.com>
2019-08-21 15:25:37 +0300

    Second Day Task + Updating ReadMe

---
+++ Day2.py
@@ -0,0 +1,17 @@
+#One line comment
+print('Hello, World!')
+
+#Comments can be placed at the end of a line
+print('Hello, World!') #One line comment
+
+#Multi line comments
+#This is comment
+#Written in more than one line
+print('Hello, World!')
+
+#Multiline String
+'''
+This is comment
+Written in more than one line
+'''
+print('Hello, World!')
---
+++ README.md
@@ -0,0 +1,4 @@
+# 100 Days of Code
+
+This initiative organized by (saudidevorg)
+
