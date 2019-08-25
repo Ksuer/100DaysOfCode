48a494d4fb65cb9d18e4b1bf4e33502d9d227706
Kuser <almaari.eman@gmail.com>
2019-08-25 23:56:10 +0300

    Day6 Task

---
+++ Day6_Casting.py
@@ -0,0 +1,26 @@
+#converting to int
+x1= int(1)
+y1= int(2.5)
+z1= int('3')
+
+print(x1)
+print(y1)
+print(z1)
+
+
+#converting to float
+x2= float(1)
+y2= float(2.5)
+z2= float('3')
+
+print(x2)
+print(y2)
+print(z2)
+
+
+#converting to string
+x3= str(1)
+y3= str(2.5)
+
+print(x3)
+print(y3)
