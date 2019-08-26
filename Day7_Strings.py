3825bcda38934a136f57307d7da4f887e5c51a6a
Kuser <almaari.eman@gmail.com>
2019-08-26 15:22:43 +0300

    Day7 Task

---
+++ Day7_Strings.py
@@ -0,0 +1,18 @@
+#We can surround string by either single quotation marks, or double quotation marks.
+print("Eman")
+print('Eman')
+
+#Assigning a string to a variable
+name= 'My name is Eman'
+print(name)
+
+#Multiline Strings
+a= '''Python is an interpreted, 
+high-level, 
+general-purpose programming language. 
+'''
+print(a)
+
+#Substring
+b='Hello, world'
+print(b[0:5])
