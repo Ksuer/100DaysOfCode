a0ba547c91ae5667eaa53edc780efc989d2d054b
Kuser <almaari.eman@gmail.com>
2019-08-30 01:00:14 +0300

    Day10 Task

---
+++ Day10_Operators.1.py
@@ -0,0 +1,25 @@
+# Arithmetic operators are used with numeric values to perform common mathematical operations.
+# (+ , -, *, /, %, **, //)
+
+print(4*2) #result 8
+print(2**2)#result 4
+
+# Assignment operators are used to assign values to variables.
+x= 9
+x &= 3
+print('The new value of x is: ',x)
+
+y= 7
+y |= 3
+print('The new value of y is: ',y)
+
+z= 5
+z ^= 3
+print('The new value of z is: ',z)
+
+# Comparison operators are used to compare two values.
+x= 10
+y= 15
+
+print(x <= y)
+print(x >= y)
