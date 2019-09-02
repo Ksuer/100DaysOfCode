9d69060c48b2e3574c0e356bd5bed8f07c447b38
Kuser <almaari.eman@gmail.com>
2019-09-02 14:28:23 +0300

    Day13 Task

---
+++ Day13_List.1.py
@@ -0,0 +1,18 @@
+# List of numbers
+numbers= [1,2,3,4,5]
+print(numbers)
+
+# Python lists can have elements of different data types
+my_list= ['a','b','c',1,2,3]
+print(my_list)
+
+# Accessing items in my_list
+print(my_list[2]) # result: c
+
+# Loop through my_list
+for i in my_list:
+    print(i)
+	
+# Changing value of item in my_list
+my_list[5]= 5
+print(my_list) # ['a', 'b', 'c', 1, 2, 5]
