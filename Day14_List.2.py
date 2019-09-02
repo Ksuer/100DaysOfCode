e381d69d86eb95234d29385df4b385c6e12c0d69
Kuser <almaari.eman@gmail.com>
2019-09-02 14:59:25 +0300

    Day14 Task

---
+++ Day14_List.2.py
@@ -0,0 +1,10 @@
+letters_list= ['a', 'b', 'c', 'd', 'e']
+# List slicing
+print(letters_list[0:3]) #['a', 'b', 'c']
+
+# To determine if a specified item is present in a list use the in keyword.
+print('f' in letters_list) #False
+
+# Repeating items in list
+a_list= ['a']*3
+print(a_list) #['a', 'a', 'a']
