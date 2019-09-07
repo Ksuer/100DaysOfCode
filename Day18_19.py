d15764c34db6b2101dc7759f82976d5aa9666b43
Kuser <almaari.eman@gmail.com>
2019-09-08 00:28:20 +0300

    Day18_19 Task

---
+++ Day18_19.py
@@ -0,0 +1,16 @@
+# Question.1
+university_majors= ['Computer Science', 'Mathematics', 'Statistics']
+
+# 1. add item
+university_majors.append('Pharmacology')
+# 2. index of item
+university_majors.index('Mathematics') #1
+# 3. reverse list
+university_majors.reverse()
+# 4. sort list
+university_majors.sort() #['Computer Science', 'Mathematics', 'Pharmacology', 'Statistics']
+
+# Question.2
+programming_languages = ("swift", "python", "java")
+if 'python' in programming_languages:
+    print("'Python' exist.") #'Python' exist.
